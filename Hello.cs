3089da8c667cf09e0fad3881bfb797e46dce3270
Даниил <belovdaniil448@gmail.com>
2026-02-25 10:35:52 +0300

    Added SayGoodbye method

--- Hello.cs
+++ Hello.cs
@@ -2,5 +2,9 @@ public static class Hello
 {
 public static void SayHello()
 {
-System.Console.WriteLine("Hello from Dev1!");
+System.Console.WriteLine("Hello from feature branch!");
+}
+public static voud SayGoodbye()
+{
+System.Console.WriteLine("Goodbye!");
 }
